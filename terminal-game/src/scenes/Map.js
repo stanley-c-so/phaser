b5216f8a70ae4b387ed0681202f754bf8f07f604
Stanley So <stanley.c.so@gmail.com>
2026-01-08 17:13:35 -0500

    WIP Creating string buffer for drawing map

--- terminal-game/src/scenes/Map.js
+++ terminal-game/src/scenes/Map.js
@@ -1,6 +1,19 @@
 import Phaser from "phaser";
 
 import { drawBorderBox } from "../utils/draw";
+import { MARGINS, TEXT_STYLE } from "../config/constants";
+
+function putStr(buf, x, y, str) {
+  const ROW_LIMIT = buf.length;
+  const COL_LIMIT = buf[0].length;
+  if (y < 0 || y >= ROW_LIMIT) return;
+  if (x + str.length - 1 >= COL_LIMIT) return;
+  for (let i = 0; i < str.length; ++i) {
+    const xx = x + i;
+    buf[y][xx] = str[i];
+  }
+}
+
 
 export default class Map extends Phaser.Scene {
   constructor() {
@@ -10,7 +23,22 @@ export default class Map extends Phaser.Scene {
   redraw() {
     this.ui?.removeAll(true);
     this.ui = this.add.container(0, 0);
+
     drawBorderBox.bind(this)("PUMP ROOM");
+
+    this.buffer = Array.from(
+      {length: this.registry.get("drawInnerAreaHeightInCells")},
+      () => Array(this.registry.get("drawInnerAreaWidthInCells")).fill("5")
+    );
+    this.ui.add(
+      this.add.text(
+        MARGINS.left + this.registry.get("cellW"),
+        MARGINS.top + this.registry.get("cellH"),
+        this.buffer.map(line => line.join("")).join("\n"),
+        TEXT_STYLE
+      )
+    );
+
   }
   
   create() {
